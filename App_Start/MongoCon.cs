68f5b148c334033d2fd054917ce0e5d1c4505a6e
apgh9897 <apgh9897@users.noreply.github.com>
2019-06-09 19:53:05 -0600

    Agregar archivos de proyecto.

---
+++ App_Start/MongoCon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MongoDB.Driver;
+
+namespace ProyectoProgramado3.App_Start
+{
+    public class MongoCon
+    {
+        MongoClient client;
+        public IMongoDatabase database;
+
+        public MongoCon()
+        {
+
+            var client = new MongoClient("mongodb://Pri:dqUHYkepawhHhlLX@tp2-shard-00-00-2drvn.mongodb.net:27017,tp2-shard-00-01-2drvn.mongodb.net:27017,tp2-shard-00-02-2drvn.mongodb.net:27017/test?ssl=true&replicaSet=TP2-shard-0&authSource=admin&retryWrites=true");
+            //var database = client.GetDatabase("LibreriaTEC-Nodo1");
+            database = client.GetDatabase("LibreriaTEC-Nodo1");
+
+
+            //(
+            //database = mongoClient.GetDatabase(ConfigurationManager.AppSettings["MongoDBName"]);
+
+        }
+    }
+}
---
+++ App_Start/Neo4jCon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Neo4j.Driver.V1;
+
+namespace ProyectoProgramado3.App_Start
+{
+    public class Neo4jCon
+    {
+        
+            private readonly IDriver _driver;
+
+            public Neo4jCon(string uri, string user, string password)
+            {
+                _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            }
+
+            public void PrintGreeting(string message)
+            {
+                using (var session = _driver.Session())
+                {
+                    var greeting = session.WriteTransaction(tx =>
+                    {
+                        var result = tx.Run("CREATE (a:Greeting) " +
+                                            "SET a.message = $message " +
+                                            "RETURN a.message + ', from node ' + id(a)",
+                            new { message });
+                        return result.Single()[0].As();
+                    });
+                    Console.WriteLine(greeting);
+                }
+            }
+
+            public void Dispose()
+            {
+                _driver?.Dispose();
+            }
+
+            //public static void Main()
+            //{
+            //    using (var greeter = new Neo4jCon("bolt://localhost:7687", "neo4j", "password"))
+            //    {
+            //        greeter.PrintGreeting("hello, world");
+            //    }
+            //}
+        
+    }
+}
